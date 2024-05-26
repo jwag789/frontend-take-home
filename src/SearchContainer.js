76f913b94bf41b8c91082633053abd566c30f862
Justin Wagner <justintwagner1@gmail.com>
2024-05-26 11:24:33 -0400

    Add dynamic theming
    Add button and functionality to change theme

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import SearchForm from './SearchForm';
-import { Paper, Typography } from '@mui/material';
+import { Paper, Typography, Button, Box } from '@mui/material';
 import SearchResults from './SearchResults';
-import { blueGrey } from '@mui/material/colors';
+import { blueGrey, deepOrange } from '@mui/material/colors';
 
 function SearchContainer() {
   const [searchResults, setSearchResults] = useState([]);
   const [searched, setSearched] = useState(false);
+  const [secondaryTheme, setSecondaryTheme] = useState(false);
 
   const handleOnSearch = (results) => {
     setSearchResults(results);
@@ -15,10 +16,13 @@ function SearchContainer() {
   }
 
   return (
-    <Paper sx={{p: 4, m: 4, backgroundColor: blueGrey[100], minHeight: 500}}>
-      <Typography variant="h2" component="h1" sx={{textAlign: 'center', mb: 2}} xs={{fontSize: 20}}>NPM Package Search</Typography>
-      <SearchForm onSearch={handleOnSearch} />
-      <SearchResults searchResults={searchResults} searched={searched} />
+    <Paper sx={{p: 4, m: 4, backgroundColor: secondaryTheme ? deepOrange[100] : blueGrey[100], minHeight: 500, display: 'flex', justifyContent: 'space-between', flexDirection: 'column'}}>
+      <Box>
+        <Typography variant="h2" component="h1" sx={{textAlign: 'center', mb: 2}} xs={{fontSize: 20}}>NPM Package Search</Typography>
+        <SearchForm onSearch={handleOnSearch} />
+        <SearchResults searchResults={searchResults} searched={searched} />
+      </Box>
+      <Button variant="outlined" sx={{mt: 2}} onClick={() => setSecondaryTheme(!secondaryTheme)}>Change theme</Button>
     </Paper>
   );
 }
